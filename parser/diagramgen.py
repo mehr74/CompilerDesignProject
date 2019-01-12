8beeb0ccfbaf082bf1d4d3215e7bef603f97728d
Mehrshad Lotfi Foroushani <lotfimehrshad@gmail.com>
2019-01-12 13:23:00 +0330

    add final changes to parser

--- parser/diagramgen.py
+++ parser/diagramgen.py
@@ -91,7 +91,7 @@ class GrammarParser:
                         if len(self.first[symbol] - self.first[nt]) > 0:
                             changed = True
                             self.first[nt] |= (
-                                self.first[symbol] - set(self.epsilon))
+                                    self.first[symbol] - set(self.epsilon))
                         if self.epsilon not in self.first[symbol]:
                             break
                     else:
@@ -120,7 +120,7 @@ class GrammarParser:
                         # The first symbol must be non-terminal
                         if symbol1 in self.nt:
                             first_sym2 = self.first[
-                                symbol2] - set([self.epsilon])
+                                             symbol2] - set([self.epsilon])
                             if len(first_sym2 - self.follow[symbol1]) > 0:
                                 changed = True
                                 self.follow[symbol1] |= first_sym2
@@ -137,7 +137,6 @@ class GrammarParser:
                             if len(self.follow[nt] - self.follow[second_last]) > 0:
                                 changed = True
                                 self.follow[second_last] |= self.follow[nt]
-
 
         for production in filter(lambda x: "->" in x, grammar.split("\n")):
             nt, rhs = production.split("->")
@@ -166,10 +165,11 @@ class GrammarParser:
                             s_mid = State("m")
                             self.edges.add(Edge(tmp, s_mid, symbol, 0, nt))
         for edge in self.edges:
-            if edge.con in self.nts :
+            if edge.con in self.nts:
                 edge.nt = 1
             self.states_edges[edge.begin.id] = [edge] if edge.begin.id not in self.states_edges \
                 else self.states_edges[edge.begin.id] + [edge]
+
     def next_token(self):
         self.cur_token = input()
         self.cur_token.lstrip()
@@ -182,28 +182,36 @@ class GrammarParser:
 
     def parse(self, cur_state):
         cur_token = self.cur_token
-        if(cur_state[0] == 'E'):
+        if (cur_state[0] == 'E'):
             return
-        else :
+        else:
             for edge in self.states_edges[cur_state]:
-                print("[ " + edge.begin.id + ", " + edge.end.id + ", '"
-                      + edge.con + "', '" + str(edge.nt) + "', '" + edge.dir + "']")
-                if edge.con == cur_token :
-                    print(cur_state + " --> " + edge.end.id + " # Token matched!")
+                # print("[ " + edge.begin.id + ", " + edge.end.id + ", '"
+                #      + edge.con + "', '" + str(edge.nt) + "', '" + edge.dir + "']")
+                if edge.con == cur_token:
+                    # print(cur_state + " --> " + edge.end.id + " # Token matched!")
+                    print(cur_token, end ="")
                     self.next_token()
                     self.parse(edge.end.id)
-                elif edge.nt == 1 and cur_token in self.first[edge.con] :
-                    print(cur_state + " --> " + self.nt_states[edge.con].id + " # non-terminal match (first)")
+                    return
+                elif edge.nt == 1 and (cur_token in self.first[edge.con] or self.epsilon in self.first[edge.con]):
+                    # print(cur_state + " --> " + self.nt_states[edge.con].id + " # non-terminal match (first)")
+                    print("{", end ="")
                     self.parse(self.nt_states[edge.con].id)
-                    cur_state = edge.end.id
-                elif edge.con == self.epsilon and cur_token in self.follow[edge.dir] :
-                    print(cur_state + " --> " + edge.end.id + " # epsilon (follow)")
+                    print("} (" + edge.con + ")", end ="")
                     self.parse(edge.end.id)
+                    return
+                elif edge.con == self.epsilon and cur_token in self.follow[edge.dir]:
+                    # print(cur_state + " --> " + edge.end.id + " # epsilon (follow)")
+                    self.parse(edge.end.id)
+                    return
+
 
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        print("{} <grammar>".format(sys.argv[0]))
+    if len(sys.argv) <= 2:
+        print("{} <grammar> <code>".format(sys.argv[0]))
     else:
         gf = open(sys.argv[1]).read()
+        cd = open(sys.argv[2]).read()
         ebnf = GrammarParser(gf)
         ebnf.begin_parse()
